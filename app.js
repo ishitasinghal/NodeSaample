d99110ed9ab53f6f3c635dd98c6b5d38a7ce47fd
ishitasinghal <ishita.singhal@nagarro.com>
2023-06-15 19:31:02 +0000

    changed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var http = require("http");
 
 http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
-   response.end('Hello From Ishita\n');
+   response.end(`Hello From ${process.env.NAME || 'Ishita'}\n`);
 }).listen(8000);
 
 console.log('Server running at http://127.0.0.1:8000/');
